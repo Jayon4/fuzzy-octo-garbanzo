b646e53de821d04e549999faef5704a48f9e6193
Jayon Smith <lelouch@Lelouch.JaySavvy>
2024-11-19 21:15:57 -0600

    Adding Welcome script

---
+++ ops1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: Jayon Smith
+# Date of latest revision: 11/18/24
+# Purpose: Print a string to the terminal
+echo "Hello, Ops."
+<<<<<<< HEAD
+#test
+=======
+
+>>>>>>> f28464dfef50237ed117988c378c63efed66a9f1
