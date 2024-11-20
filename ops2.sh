1ccbce19071c1086009b5bd43d25bde68e999190
Jayon Smith <lelouch@Lelouch.JaySavvy>
2024-11-19 21:23:45 -0600

    Adding Welcome script

---
+++ ops2.sh
@@ -0,0 +1,10 @@
+# #!bin/bash
+# Declare a variable with as your name
+# Print to the screen Welcome to the Class Name(variable)
+
+name1="Ben"
+dog="Lucy"
+car="Ford Focus"
+name2="Jules"
+
+echo "Welcome to the Class $name"
