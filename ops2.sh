e8036b6a9e72d4e9a0aaa53a394e3eb067dfdcf0
Jayon Smith <153973081+Jayon4@users.noreply.github.com>
2024-11-20 15:43:40 -0600

    Update ops2.sh

--- ops2.sh
+++ ops2.sh
@@ -2,9 +2,9 @@
 # Declare a variable with as your name
 # Print to the screen Welcome to the Class Name(variable)
 
-name1="Ben"
-dog="Lucy"
-car="Ford Focus"
-name2="Jules"
+name1="Jayon"
+dog="Ghost"
+car="Pagani"
+name2="Astro boy"
 
 echo "Welcome to the Class $name"
