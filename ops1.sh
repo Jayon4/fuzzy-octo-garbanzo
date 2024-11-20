f28464dfef50237ed117988c378c63efed66a9f1
Jayon Smith <153973081+Jayon4@users.noreply.github.com>
2024-11-19 20:07:57 -0600

    Add files via upload

---
+++ ops1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: Jayon Smith
+# Date of latest revision: 11/18/24
+# Purpose: Print a string to the terminal
+echo "Hello, Ops."
+
